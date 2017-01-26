b9e7f079fff8450c795eda752d0ea9218ae4ee04
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-01-26 15:19:38 +0100

    indent

--- web-tp3/Exercice-33.js
+++ web-tp3/Exercice-33.js
@@ -2,34 +2,34 @@ window.onload=function(){
 	var element = document.getElementById("ltheme");
 	element.addEventListener("change", function(){switchTheme();});
 
-  var checkbox = document.getElementById("showMenu");
-  checkbox.addEventListener("click", switchMenu);
-  
-  initialize();
+	var checkbox = document.getElementById("showMenu");
+	checkbox.addEventListener("click", switchMenu);
+	
+	initialize();
 }
 
 function switchMenu(){
 
-  var checkbox = document.getElementById("showMenu");
-  var menu = document.getElementById("menu");
+	var checkbox = document.getElementById("showMenu");
+	var menu = document.getElementById("menu");
 
-  if(checkbox.checked) {
-    menu.style.display="";
-  } else {
-    menu.style.display="none";
-  }
+	if(checkbox.checked) {
+		menu.style.display="";
+	} else {
+		menu.style.display="none";
+	}
 }
 
 function switchTheme(){
 	var element = (document.getElementsByTagName("body"))[0];
 	element.className = document.getElementById("ltheme").value;
 
-  var checkbox_span = document.getElementById("showMenu").parentNode;
-  if(document.getElementById("ltheme").value == "theme1") {
-    checkbox_span.style.display="";
-  } else {
-    checkbox_span.style.display="none";
-  }
+	var checkbox_span = document.getElementById("showMenu").parentNode;
+	if(document.getElementById("ltheme").value == "theme1") {
+		checkbox_span.style.display="";
+	} else {
+		checkbox_span.style.display="none";
+	}
 }
 
 function initialize() {
