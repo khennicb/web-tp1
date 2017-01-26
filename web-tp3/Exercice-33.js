1230512a289fe259d1c9b4f9491ce5b8f9200bf1
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-01-26 15:08:00 +0100

    initialize

--- web-tp3/Exercice-33.js
+++ web-tp3/Exercice-33.js
@@ -1,35 +1,17 @@
 window.onload=function(){
-	/*
-	var body = document.getElementsByTagName("body")[0];
-	var newList = document.createElement("ul");
-	var newElem = [];
-	for (var i = 0; i<3; i++) {
-	  newElem[i] = document.createElement("li");
-	}
-	newElem[0].appendChild(document.createTextNode("one"));
-	newElem[1].appendChild(document.createTextNode("two"));
-	newElem[2].appendChild(document.createTextNode("three"));
-
-	newList.appendChild(newElem[0]);
-	newList.appendChild(newElem[1]);
-	newList.appendChild(newElem[2]);
-
-	body.appendChild(newList);
-	*/
-
 	var element = document.getElementById("ltheme");
 	element.addEventListener("change", function(){switchTheme();});
 
   var checkbox = document.getElementById("showMenu");
   checkbox.addEventListener("click", switchMenu);
-  switchMenu();
+  
+  initialize();
 }
 
 function switchMenu(){
 
   var checkbox = document.getElementById("showMenu");
   var menu = document.getElementById("menu");
-  console.log("switchMenu\n");
 
   if(checkbox.checked) {
     menu.style.display="";
@@ -41,4 +23,9 @@ function switchMenu(){
 function switchTheme(){
 	var element = (document.getElementsByTagName("body"))[0];
 	element.className = document.getElementById("ltheme").value;
+}
+
+function initialize() {
+	switchMenu();
+	switchTheme();
 }
