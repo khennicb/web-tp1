a813a84d5ea12f1b9d407a5478d3a3507ac12f49
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-01-26 14:40:17 +0100

    commentary

--- web-tp3/Exercice-32.js
+++ web-tp3/Exercice-32.js
@@ -1,22 +1,23 @@
 window.onload=function(){
+	// Bouton 1
 	document.getElementById("b1").addEventListener("click", function() {
 		var element = document.getElementById("contents");
 		element.style.backgroundColor = "yellow";
 	});
 
-	var defaultColor = getComputedStyle((document.getElementsByTagName("h1"))[0]).color;
-
+	// Bouton 2
 	document.getElementById("b2").addEventListener("click", function() {
 		var element = document.getElementsByTagName("h1");
 		for (var i = element.length - 1; i >= 0; i--) {
 			if(getComputedStyle(element[i]).color != 'rgb(255, 0, 0)'){
 				element[i].style.color = "red";
 			}else{
-				element[i].style.color = defaultColor;
+				element[i].style.color = '';
 			}
 		}
 	});
 
+	// Bouton 3
 	document.getElementById("b3").addEventListener("click", function() {
 		var element = document.getElementsByTagName("p");
 		var i = 0;
