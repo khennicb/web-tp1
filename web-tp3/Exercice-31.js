375e8e7b1924adf28712a0a0757b6ca99474ecfe
khennicb <benkhen@hotmail.fr>
2017-01-26 14:32:19 +0100

    job done bitches

--- web-tp3/Exercice-31.js
+++ web-tp3/Exercice-31.js
@@ -1,5 +1,20 @@
+window.onload=function(){
+
+var body = document.getElementsByTagName("body")[0];
+var newList = document.createElement("ul");
+var newElem = [];
+for (var i = 0; i<3; i++) {
+  newElem[i] = document.createElement("li");
+}
+newElem[0].appendChild(document.createTextNode("one"));
+newElem[1].appendChild(document.createTextNode("two"));
+newElem[2].appendChild(document.createTextNode("three"));
+
+newList.appendChild(newElem[0]);
+newList.appendChild(newElem[1]);
+newList.appendChild(newElem[2]);
 
 
-var body = document.getElementsByTagName("body");
-var newElement = document.createElement("ul");
-body.appendChild(newElement);
+body.appendChild(newList);
+
+}
