72475d8ef8e5cd38e065e4a28ad43eb8b46f0b46
Benjamin Khenniche <benjamin.khenniche@ensimag.grenoble-inp.fr>
2017-02-02 15:30:28 +0100

    ptit fix pour que l'ajout d'une classe se fasse proprement

--- web-tp3/Exercice-36.js
+++ web-tp3/Exercice-36.js
@@ -27,13 +27,13 @@ function color_marks (){
 		var mark = mark_field.textContent;
 
 		if (mark < 8) {
-			mark_field.className += "mark-0-8";
+			mark_field.className += " mark-0-8";
 		}else	if (mark < 10) {
-			mark_field.className += "mark-8-10";
+			mark_field.className += " mark-8-10";
 		}else	if (mark < 12) {
-			mark_field.className += "mark-10-12";
+			mark_field.className += " mark-10-12";
 		} else {
-			mark_field.className += "mark-12-20";
+			mark_field.className += " mark-12-20";
 		}
 
 	}
@@ -65,12 +65,9 @@ function triTable () {
     var ligne = tbody.rows;
     var nNbrLigne = ligne.length;
     var colonne = new Array();
+    
     for(var i = 0; i < nNbrLigne; i++) {
-    	var cellule = ligne[i].cells;
-    	colonne[i] = new Array();
-    	for(var j = 0; j < cellule.length; j++){
-    		colonne[i][j] = cellule[j].innerHTML;
-    	}
+        colonne[i] = ligne[i];
     }
 
     console.log(colonne);
@@ -89,9 +86,12 @@ function triTable () {
 
     // evalue les elements de la matrice
     function triAlphaElems (a, b) {
-    	if(a[index] < b[index]){
+        var a_value = a.children[index].innerText;
+        var b_value = b.children[index].innerText;
+
+    	if(a_value < b_value){
     		return -1;
-    	}else if (a[index] == b[index]) {
+    	}else if (a_value == b_value) {
     		return 0;
     	}else{
     		return 1;
@@ -100,7 +100,10 @@ function triTable () {
 
     // evalue les elements de la matrice
     function triNumElems (a, b) {
-    	return a[index] - b[index];
+        var a_value = a.children[index].innerText;
+        var b_value = b.children[index].innerText;
+
+    	return a_value - b_value;
     }
 
     // Tri decroissant si necessaire
@@ -111,14 +114,15 @@ function triTable () {
     console.log(colonne);
 
     // Construit les colonne du nouveau tableau
+    var bodyt_to_insert = "";
     for(i = 0; i < nNbrLigne; i++){
-    	colonne[i] = "<td>"+colonne[i].join("</td><td>")+"</td>";
+    	bodyt_to_insert += colonne[i].outerHTML;
     }
 
+    console.log(bodyt_to_insert);
+
     // assigne les lignes au tableau
-    tbody.innerHTML = "<tr>"+colonne.join("</tr><tr>")+"</tr>";
+    tbody.innerHTML = bodyt_to_insert;
 
 
-    // TODO : Trouver solution pour s'en passer
-    color_marks();
 }
