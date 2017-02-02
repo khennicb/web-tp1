f392e181bdfc61e1761cf200d446ca81e0d26b6a
NonoDesBois <noon.38@free.fr>
2017-02-02 14:50:42 +0100

    ex1-3

--- web-tp4/Exercice-41.js
+++ web-tp4/Exercice-41.js
@@ -3,16 +3,17 @@ window.addEventListener("load",load);
 function load () {
 	cacherVide();
 	colorerLignes();
+	nombres();
 }
 
 function cacherVide () {
 	$("tbody").each(estVide);
 
 	function estVide () {
-	if ($(this).children().size() == 0) {
-		$(this).parents("div").css("display", "none");
-	};
-}
+		if ($(this).children().size() == 0) {
+			$(this).parents("div").css("display", "none");
+		}
+	}
 }
 
 
@@ -36,4 +37,9 @@ function colorerLignes () {
 			index++;
 		}
 	}
+}
+
+
+function nombres () {
+	$("tbody > tr > td:nth-child(2), tbody > tr > td:nth-child(3)").addClass("nombre");
 }
