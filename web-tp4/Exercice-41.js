81780c3cbeea92e4c33112f68d1e194c71d4844f
NonoDesBois <noon.38@free.fr>
2017-02-02 14:42:04 +0100

    ex1-2

--- web-tp4/Exercice-41.js
+++ web-tp4/Exercice-41.js
@@ -2,14 +2,38 @@ window.addEventListener("load",load);
 
 function load () {
 	cacherVide();
+	colorerLignes();
 }
 
 function cacherVide () {
-	$('tbody').each(estVide);
-}
+	$("tbody").each(estVide);
 
-function estVide () {
+	function estVide () {
 	if ($(this).children().size() == 0) {
 		$(this).parents("div").css("display", "none");
 	};
+}
+}
+
+
+
+function colorerLignes () {
+	$("tbody").each(coloreMyLines);
+
+	function coloreMyLines () {
+		var index = 0;
+		$(this).find("tr").each(coloreOneLine);
+
+		function coloreOneLine () {
+			if (index % 3 == 0) {
+				$(this).addClass("lig1");
+			}else if (index % 3 == 1) {
+				$(this).addClass("lig2");
+			}else{
+				$(this).addClass("lig3");
+			}
+
+			index++;
+		}
+	}
 }
