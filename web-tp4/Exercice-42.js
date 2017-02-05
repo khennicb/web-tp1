b7329407ab4c0092ee817e3c33f20edcb5bf620b
khennicb <benkhen@hotmail.fr>
2017-02-05 17:49:20 +0100

    j'ai fait un changement qui rend le truc compatible avec firefox linux. jvais voir si ca passe aussi sur windows é

--- web-tp4/Exercice-42.js
+++ web-tp4/Exercice-42.js
@@ -1,5 +1,7 @@
 $(function() {
-//	set_color_regarding_mark.apply($("tbody > tr > td:nth-child(6)"), null)
+
+	initTri();
+
 	$("tbody > tr > td:nth-child(6)").each(function( index ){
 
 		var mark = $(this).text();
@@ -20,3 +22,96 @@ $(function() {
 
 
 
+
+// Initialise la fonction pour les tris
+function initTri () {
+	var ths = document.getElementsByTagName("th");
+
+	for (var i = 0; i < ths.length; i++) {
+		ths[i].addEventListener("click", triTable);
+		ths[i].setAttribute("col-pos", i);
+		ths[i].setAttribute("tri", 1);
+		if (i == 0 || i == 4 || i == 5 || i == 7) {
+			ths[i].setAttribute("type", "num");
+		}else{
+			ths[i].setAttribute("type", "alpha-num");
+		}
+	}
+}
+
+function triTable () {
+	var tri = this.getAttribute("tri");
+	this.setAttribute("tri", (tri=="0") ? "1" : "0");
+
+
+	// Construit la matrice
+    // Récupère le tableau (tbody)
+    var tbody = document.getElementById("T-1").getElementsByTagName("tbody")[0];
+    var ligne = tbody.rows;
+    var nNbrLigne = ligne.length;
+    var colonne = new Array();
+
+    for(var i = 0; i < nNbrLigne; i++) {
+        colonne[i] = ligne[i];
+    }
+
+    console.log(colonne);
+
+    // Recuper le  numero de la colone a trier
+    var index = this.getAttribute("col-pos");
+		console.log(index);
+
+    // Recupere le type de tri (alpha numerique ou numerique)
+    if (this.getAttribute("type") == "num") {
+    	colonne.sort(triNumElems);
+    }else{
+    	colonne.sort(triAlphaElems);
+    }
+
+    console.log(colonne);
+
+    // evalue les elements de la matrice
+    function triAlphaElems (a, b) {
+        var a_value = a.children[index].innerHTML;
+        var b_value = b.children[index].innerHTML;
+
+				console.log(a_value);
+				console.log(b_value);
+
+    	if(a_value < b_value){
+    		return -1;
+    	}else if (a_value == b_value) {
+    		return 0;
+    	}else{
+    		return 1;
+    	}
+    }
+
+    // evalue les elements de la matrice
+    function triNumElems (a, b) {
+        var a_value = a.children[index].innerHTML;
+        var b_value = b.children[index].innerHTML;
+
+    	return a_value - b_value;
+    }
+
+    // Tri decroissant si necessaire
+    if (tri == "0") {
+    	colonne.reverse();
+    }
+
+    console.log(colonne);
+
+    // Construit les colonne du nouveau tableau
+    var bodyt_to_insert = "";
+    for(i = 0; i < nNbrLigne; i++){
+    	bodyt_to_insert += colonne[i].outerHTML;
+    }
+
+    //console.log(bodyt_to_insert);
+
+    // assigne les lignes au tableau
+    tbody.innerHTML = bodyt_to_insert;
+
+
+}
