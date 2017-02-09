07f0a55834d15553fb2d6840371a3ff30d11099e
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-02-09 15:47:11 +0100

    ex4

--- web-tp5/ServeurEx50/webapps/bmt/appli/Exercice-50.js
+++ web-tp5/ServeurEx50/webapps/bmt/appli/Exercice-50.js
@@ -40,11 +40,27 @@ function selectObjectType(type) {
 function listBookmarks() {
 	$('#items').empty();
 	var url = 'http://localhost:8080/bmt/foreyn-khennicb/bookmarks';
-	jQuery.get(url, printBookmarks);
+	jQuery.getJSON(url, printBookmarks).fail(function(){console.log('error :(')});
 }
 
 function printBookmarks(data){
-	console.log(data);
+	$(data).each(insertBookmark);
+}
+
+function insertBookmark(){
+	// console.log(this);
+	var copyBook = $('.model.bookmark').clone();
+	copyBook.find('h2').append(this['title']);
+	copyBook.find('a').append(this['link']);
+	copyBook.find('a').append(this['link']);
+
+	var tagFather = copyBook.find('.tags');
+	for (var i = 0; i < this['tags'].length; i++) {
+		tagFather.append('<li>' +this['tags'][i]['name'] +'</li>');
+	}
+
+	copyBook.toggleClass('model item');
+	$('#items').append(copyBook);
 }
 
 /* Loads the list of all tags and displays them */
