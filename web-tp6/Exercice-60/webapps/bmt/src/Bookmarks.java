061be5a04f00cc650e1032fc74fa7875983dc925
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-03-02 15:58:22 +0100

    ex3 wip

--- web-tp6/Exercice-60/webapps/bmt/src/Bookmarks.java
+++ web-tp6/Exercice-60/webapps/bmt/src/Bookmarks.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +10,7 @@ public class Bookmarks {
 	
 	public static void handleBookmarksList(HttpServletRequest req, HttpServletResponse resp,
 			Dispatcher.RequestMethod method, String[] requestPath,
-			Map<String, List<String>> queryParams, User user) {
+			Map<String, List<String>> queryParams, User user) throws IOException {
 
 		// Rule-out PUT and DELETE requests
 		System.out.println("Action: handleBookmarksList - " + method + "-" + queryParams);
@@ -20,6 +22,33 @@ public class Bookmarks {
 		// Handle GET
 		if (method == Dispatcher.RequestMethod.GET){
 			List<Bookmark> bookmarks = null;
+			try{
+				bookmarks = BookmarkDAO.getBookmarks(user);
+			} catch (SQLException ex) {
+				resp.setStatus(500);
+				return;
+			}
+			
+			// Encode the tag list to JSON
+			String json = "[";
+			for (int i = 0, n = bookmarks.size(); i < n; i++) {
+				Bookmark book = bookmarks.get(i);
+				json += book.toJson();
+				if (i < n - 1)
+					json += ", ";
+			}
+			json += "]";
+			System.out.println(json);
+			// Send the response
+			resp.setStatus(200);
+			resp.setContentType("application/json");
+			resp.getWriter().print(json);
+			return;
+		}
+		
+		// Handle POST
+		if (method == Dispatcher.RequestMethod.POST){
+			
 		}
 	}
 
